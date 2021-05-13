74de1cf8bd9d33225a1f974c0b7320b917d6a12c
Stefanos Karamperas <stef1997s@gmail.com>
2021-05-13 04:40:55 +0300

    Fixed a mistake in the implementation of HashFunction regarding the "w" parameter and the calculation of the random bias.

--- src/core/lsh/HashFunction.java
+++ src/core/lsh/HashFunction.java
@@ -3,21 +3,33 @@ package core.lsh;
 import java.util.Random;
 
 public class HashFunction {
+    private final Random randomGenerator;
     private final Entry randomVector;
     private final double randomBias;
     private final double w;
 
-    public HashFunction(int dimensions, double w) {
-        Random rand = new Random();
+    public HashFunction(int dimensions, int w) {
+        randomGenerator = new Random();
         this.w = w;
-        this.randomBias = rand.nextDouble();
+        this.randomBias = getRandomDouble(0, w);
 
         double[] randomValues = new double[dimensions];
         for(int d = 0; d < dimensions; d ++) {
-            randomValues[d] = rand.nextGaussian();
+            randomValues[d] = randomGenerator.nextGaussian();
         }
 
         randomVector = new Entry(randomValues);
+    }
+
+    private double getRandomDouble(int upperBound, int lowerBound) {
+        // Note that the upper and lower bounds are inclusive.
+        if (lowerBound < 0 || upperBound < 0) {
+            throw new IllegalArgumentException("Lower and upper bounds must be equal or greater than 0.");
+        } else if (upperBound <= lowerBound) {
+            throw new IllegalArgumentException("Upper bound must be greater than lower bound.");
+        }
+
+        return randomGenerator.nextDouble() * (upperBound - lowerBound) + lowerBound;
     }
 
     public int hash(Entry vector){
